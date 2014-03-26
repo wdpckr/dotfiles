0da72d7094a46fbfd1ec79c5a732ea175d7944c1
wdpckr <wdpckr@gmx.net>
2014-03-26 13:33:23 +0100

    updated prompt for user root

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -20,7 +20,12 @@ zstyle ':vcs_info:*' formats ':%b%c%u%f'
 zstyle ':vcs_info:(sv[nk]|bzr):*' branchformat '%b%F{1}:%F{3}%r'
 precmd () { vcs_info }
 
-PROMPT='[%n@%m $(sub_prompt)%F{4}%~$(git_color)${vcs_info_msg_0_}%f] %{$reset_color%}'
+user=`whoami`
+if [[ "$user" == "root" ]]; then
+	PROMPT='%F{1}[%n@%m%f $(sub_prompt)%F{4}%~$(git_color)${vcs_info_msg_0_}%f%F{1}]%f %{$reset_color%}'
+else
+	PROMPT='[%n@%m $(sub_prompt)%F{4}%~$(git_color)${vcs_info_msg_0_}%f] %{$reset_color%}'
+fi
 
 function git_color() {
 	if [[ -n $(git status -s 2> /dev/null) ]]; then
