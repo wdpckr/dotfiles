beb540e43efa5a7b82ab43fd0f99598777d8077b
wdpckr <wdpckr@gmx.net>
2014-08-19 18:21:56 +0200

    changed additional route configuration for vagrant linux clients

--- .zsh/plugin/linux/route.zsh
+++ .zsh/plugin/linux/route.zsh
@@ -1,19 +1,13 @@
-INTERFACES=(
-	"eth0"
-	"eth1"
-	"eth2"
-)
 
-for (( i=1; i<=${#INTERFACES[@]}; i=i+1)); do
-	ETHxLINK=`sudo mii-tool | grep "${INTERFACES[i]}" | grep "link ok"`	 
+for (( i=2; i<=`sudo mii-tool | wc -l`; i=i+1)); do
+	IF=`sudo mii-tool | awk NR==$i'{print $1}' | cut -d':' -f-1`
+	ETHxLINK=`sudo mii-tool | grep "${IF}" | grep "link ok"`	 
 	if  [ "$ETHxLINK" != "" ]; then
-		ETHxIP=`/sbin/ifconfig ${INTERFACES[i]} | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}'`
+		ETHxIP=`/sbin/ifconfig ${IF} | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}'`
 		ETHxIPp=`echo "$ETHxIP" | cut -d'.' -f-2`
-		if [[ "$ETHxIPp" == "10.241" ]]; then
-			ROUTEEXIST=`ip route show 10.241.0.0/16 | wc -l`
-			if [ $ROUTEEXIST -eq 0 ]; then
-				sudo ip route add 10.241.0.0/16 dev ${INTERFACES[i]}
-			fi
+		ROUTEEXIST=`ip route show ${ETHxIPp}.0.0/16 | wc -l`
+		if [ $ROUTEEXIST -eq 0 ]; then
+			sudo ip route add ${ETHxIPp}.0.0/16 dev ${IF}
 		fi
 	fi
 done
